1d12c5c5ca23638972c616a92331bb00b94d00df
Bomin Seo <qhals8775@naver.com>
2022-12-26 12:39:06 +0900

    Add files via upload

---
+++ js/StarShip.js
@@ -0,0 +1,14 @@
+//  =========== 박스 슬라이더 ===============
+$('document').ready(function(){
+    $('.slider_img').bxSlider({
+        mode: 'fade',
+        auto: true,
+        autoControls: false,
+        autoHover: false,
+        pager: false,
+        captions: true,
+        // centeredSlides: true,
+        // slideWidth: 1920,
+        
+    });
+  });
